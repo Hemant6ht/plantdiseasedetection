805eea1a63f05108829f1ba10014405f03d7a6cf
Hemant6ht <55032852+Hemant6ht@users.noreply.github.com>
2020-06-25 18:39:19 +0530

    initial commit

---
+++ static/javascript/hamut.js
@@ -0,0 +1,8 @@
+const navslide = () => {
+	const burger = document.querySelector('.burger');
+	const nav = document.querySelector('.nav-link');
+	burger.addEventListener('click',() => {
+		nav.classList.toggle('nav-active');
+	})
+}
+navslide();
